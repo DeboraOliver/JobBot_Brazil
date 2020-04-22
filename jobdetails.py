84c33da41e4cf08f32d82ac27e57a3a1c08ffdb5
Debora Oliveira <debora.ufabc@gmail.com>
2020-04-22 15:06:04 -0300

    My second commit

--- jobdetails.py
+++ jobdetails.py
@@ -8,12 +8,12 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import time, random, os, csv, datetime
 
-
 class WebScrapJob:
+
     def __init__(self, url):
         print (url)
         self.url = url
-		# url = "https://www.infojobs.com.br/vaga-de-desenvolvedor-python-junior-em-sao-paulo__6805056.aspx"
+        # url = "https://www.infojobs.com.br/vaga-de-desenvolvedor-python-junior-em-sao-paulo__6805056.aspx"
         self.uClient = uReq (self.url)
         self.page_html = self.uClient.read ()
         self.uClient.close ()  # fecha o pedido anterior qndo eu terminar
@@ -22,7 +22,7 @@ class WebScrapJob:
         self.page_soup = soup (self.page_html, "html.parser")
 
         # grabs each vacancy
-        #self.containers = self.page_soup.findAll ("div", {"class": "element-vaga"}) #NÃO EXISTE
+        # self.containers = self.page_soup.findAll ("div", {"class": "element-vaga"}) #NÃO EXISTE
         self.save ()
 
     def save(self):
@@ -31,52 +31,53 @@ class WebScrapJob:
         self.f = open (self.filename, "w")
 
         headers = "vaga,empresa,salário,data,cidade,estado,link\n"  # csv sao definidos pelo \n
-        self.f.write (headers)
+        self.f.write(headers)
         self.jobs ()
 
-
     def jobs(self):
-        #for container in self.containers:
-		# VAGA
-		self.vaga_page = page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litVacancyTitle"})
-		self.vaga = self.vaga_page[0].text.strip ()
+        # for container in self.containers:
+        # VAGA
+        self.vaga_page = self.page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litVacancyTitle"})
+        self.vaga = self.vaga_page[0].text.strip ()
 
-		# EMPRESA
-		self.empresa_page = page_soup.findAll ("a", {"id": "ctl00_phMasterPage_cVacancySummary_aCompany"})
-		self.empresa = self.empresa_page[0].text.strip ()
+        # EMPRESA
+        self.empresa_page = self.page_soup.findAll ("a", {"id": "ctl00_phMasterPage_cVacancySummary_aCompany"})
+        self.empresa = self.empresa_page[0].text.strip ()
 
-		# Salário
-		self.salario_page = page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litSalary"}) 
-		self.salario = self.salario_page[0].text.strip ()
+        # Salário
+        self.salario_page = self.page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litSalary"})
+        self.salario = self.salario_page[0].text.strip ()
 
-		# CIDADE
-		self.cidade_page = page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litLocation"})
-		self.cidade = self.cidade_page[0].text.strip () 
-		
-		# ESTADO
-		self.estado_page = page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litLocation"})
-		self.estado = self.estado_page[0].text.strip ()
-		self.estado = self.estado[-3:-1]
-		
-		# self.estado = self.estado.replace ("\n", "")
+        # CIDADE
+        self.cidade_page = self.page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litLocation"})
+        self.cidade = self.cidade_page[0].text.strip ()
 
-		# LINK
-		self.link = url
+        # ESTADO
+        self.estado_page = self.page_soup.findAll ("span", {"id": "ctl00_phMasterPage_cVacancySummary_litLocation"})
+        self.estado = self.estado_page[0].text.strip ()
+        self.estado = self.estado[-3:-1]
 
-		
-		print ("Vaga: " + self.vaga)
-		print ("Empresa:" + self.empresa)
-		print ("Area: " + self.salario)
-		print ("Data: " + self.data)
-		print ("Cidade: " + self.cidade)
-		print ("Estado: " + self.estado)
-		print ("Link: " + self.link)
+        # self.estado = self.estado.replace ("\n", "")
 
-		self.f.write (self.vaga + "," + self.empresa.replace (",", "|") + "," +  self.salario.replace (",", "|") + "," +  self.data + "," + self.cidade.replace (",", "|") + "," + self.estado + "," +self.link + "\n")
-		# f.write(vaga + "," + empresa.replace(",", "|") + "," + area.replace(",","|") + "," + data + "," + local.replace("-",",") + "," + atividade.replace(",", "|") + "," + link + "\n")
+        # DATA
+        self.data = datetime.datetime.now()
 
-	self.f.close ()
+        # LINK
+        # self.link = url
 
 
+        print ("Vaga: " + self.vaga)
+        print ("Empresa:" + self.empresa)
+        print ("Area: " + self.salario)
+        print ("Data: " + self.data)
+        print ("Cidade: " + self.cidade)
+        print ("Estado: " + self.estado)
+        print ("Link: " + self.url)
 
+        self.f.write (self.vaga + "," + self.empresa.replace (",", "|") + "," + self.salario.replace (",",
+                                                                                                      "|") + "," + self.data + "," + self.cidade.replace (
+            ",", "|") + "," + self.estado + "," + self.url + "\n")
 
+        # f.write(vaga + "," + empresa.replace(",", "|") + "," + area.replace(",","|") + "," + data + "," + local.replace("-",",") + "," + atividade.replace(",", "|") + "," + link + "\n")
+
+        self.f.close()
--- real.py
+++ real.py
@@ -27,80 +27,100 @@ class InfoJob:
 
         self.driver.get ('https://www.infojobs.com.br/login.aspx?vurl=%2fcandidate%2f')
 
-        #self.vaga = vaga  # enter username and password
+        # self.vaga = vaga  # enter username and password
         self.driver.find_element_by_xpath ('//*[@id="ControlLogin_txtLogin"]').send_keys (email)
-        elf.driver.find_element_by_xpath ('//*[@id="ControlLogin_txtPwd"]').send_keys (senha)
+        self.driver.find_element_by_xpath ('//*[@id="ControlLogin_txtPwd"]').send_keys (senha)
         self.driver.find_element_by_xpath ('//*[@id="ControlLogin_btnValidar"]').click ()
 
-        self.vacancy_search (vaga)
+        self.vacancy_search(vaga)
+
+    # CLIENT MIGHT WANT TO APPLY USING A RANGE OF JOB TITLES ----> Loop over a list
 
     def vacancy_search(self, vaga):
         print ('Vamos encontrar uma vaga')
 
         # type the vacancy ------------> either user enter or a GUI could be used
-        self.driver.find_element_by_xpath (         '//*[@id="aspnetForm"]/div[4]/div[6]/section[1]/div/div/ol/li[1]/input').send_keys (vaga)
-		
-        self.driver.find_element_by_xpath ('//*[@id="aspnetForm"]/div[4]/div[6]/section[1]/div/div/ol/li[3]/button').click ()
+        self.driver.find_element_by_xpath ('//*[@id="aspnetForm"]/div[4]/div[6]/section[1]/div/div/ol/li[1]/input').send_keys(vaga)
+        #//*[@id="aspnetForm"]/div[4]/div[6]/section[1]/div/div/ol/li[1]/input
+        self.driver.find_element_by_xpath('//*[@id="aspnetForm"]/div[4]/div[6]/section[1]/div/div/ol/li[3]/button').click ()
         self.filtering ()
 
         # filter by cities, level and salary ---> it shouldn't be here. A GUI might be a better option
 
     def filtering(self):
         try:
-            # filtering by State
+            # filtering by State ----> state should be a variable 
             self.driver.find_element_by_link_text ('Santa Catarina').click ()
+            # we don't wanna work in SP right?!
+            self.driver.find_element_by_link_text ('São Paulo').click ()
             # PROBLEM: I can't click on the one that came by default
             time.sleep (random.uniform (1.5, 3.5))
-            # filtering by salary rate
+            # filtering by salary rate ---> it should be asked
             self.driver.find_element_by_partial_link_text ('5.000,00').click ()
+            print ("Filtros online")
 
         except:
-            print ("Não deu para colocar os filtros!")
+            print ("Não deu para colocar os filtros!")  # message in case of failure
 
         time.sleep (random.uniform (2.5, 3.5))
         self.apply ()
 
+    # criar uma outra função que puxa a lista de url anteriores e bate com as que temos
+
     def apply(self):
-        self.job_list = self.driver.find_elements_by_class_name ('location2')
+        # mudar a lista a seguir para que elapegue apenas as vagas que 1 - não foram listadas antes; 2 - que não exijam assinatura
+
+        # lista de vagas na primeira pagina
+        elems = self.driver.find_elements_by_css_selector (".vaga [href]")
+        self.links_job = [elem.get_attribute ('href') for elem in elems]
+
+        jobs_with_futher_questions = 0
         jobs_applied = 0
 
-        for x in self.job_list:
-            x.click ()
+        for x in self.links_job:
+            print(x)
+            x.click()#CHANGE IT
 
             try:
-                self.driver.find_element_by_link_text ('Assine aqui!')
-                time.sleep (random.uniform (2, 3))
-                print ("Vaga só para assinantes!")
-                pass
-            except:
                 url = self.driver.current_url
                 print (url)
                 time.sleep (random.uniform (1, 2.5))
                 # entrar na classe que salva tudo
                 app = jobdetails.WebScrapJob (url)
-
                 candidatar = self.driver.find_element_by_id ("ctl00_phMasterPage_cHeader_lnkCandidatar")
                 candidatar.click ()
-                # CONCLUIR CANDIDATURA
-                # confirming = self.driver.find_element_by_id ('ctl00_phMasterPage_btnInsertMatch')
-                try:
-                    self.driver.find_element_by_id ('ctl00_phMasterPage_btnInsertMatch')
+                # HÁ UMA EXCEÇÃO AQUI
+                if self.driver.find_element_by_xpath ('//*[@id="ctl00_phMasterPage_btnInsertMatch"]'):
                     print ("Desta vez não deu")
+                    jobs_with_futher_questions += 1
                     time.sleep (random.uniform (1.5, 2.5))
                     pass
-                except:
+                else:
                     print ("Candidatura realizada!")
                     jobs_applied += 1
                     print (jobs_applied)
                     time.sleep (random.uniform (2.5, 3.5))
 
-                    # TALVEZ SEJA PRECISO CRIAR UMA NOVA CLASSE PARA LIDAR COM OUTRAS EXCESSOES
+            except:
+                self.driver.find_element_by_link_text ('Assine aqui!')
+                time.sleep (random.uniform (2, 3))
+                print ("Vaga só para assinantes!")
+                pass
+
+# TALVEZ SEJA PRECISO CRIAR UMA NOVA CLASSE PARA LIDAR COM OUTRAS EXCESSOES
 
 
 email = input ("Seu email: ")
 senha = input ("Sua senha: ")
+# estado = input("Estado: ")
+# A list of cities:
+# cidade1 = input("Cidades (separedas vírgulas: ")
+# cidade = cidade1.split(',')
+# A list of job positions you might fit in
 vaga = input ("Vaga: ")
-
+# vaga1 = input ("Vagas (separe com vírgula): ")
+# vaga = vaga1.split(',')
+# print("Suas cidades: ", vaga)
 teste = InfoJob (email, senha, vaga)
 
 """para iniciar no cmd  use 
